66c03d71b81b2aac2e3b97c6c2412506dd6b28c5
Teja <teja90.exe@gmail.com>
2020-01-30 15:00:11 -0800

    Delete img.py

--- src/img.py
+++
@@ -1,9 +0,0 @@
-
-from skimage import io, transform
-from skimage.color import gray2rgb
-
-image = io.imread('data/train/5935a672-23d2-11e8-a6a3-ec086b02610b.jpg')  #problem image
-print(io.imshow(image))
-
-image = io.imread('data/train/58767ee4-23d2-11e8-a6a3-ec086b02610b.jpg')
-print(io.imshow(image))
